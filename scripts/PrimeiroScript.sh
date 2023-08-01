3e806bcb780f56bb5f2d59188bebed641301af51
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-01 14:47:36 -0300

    Criação do primeiro script

---
+++ scripts/PrimeiroScript.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+clear
+
+echo "============MEU PRIMEIRO SCRIPT============"
+echo ""
+echo -n "Exibir data atual: "
+date
+echo "==========================================="
+echo ""
+echo "Listagem das questões da atividade 17 do curso \"Java básico - Loiane\": "
+ls -a ~/cursoJava_Loiane/atividade_aula17/src/
