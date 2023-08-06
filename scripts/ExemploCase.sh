25e8abac0501389f35e52ca8c6bb6d12457720a3
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-06 19:19:36 -0300

    continuação do conteúdo da aula 30 do curso

---
+++ scripts/ExemploCase.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+case "$1" in
+  [0-9])
+    echo "O parâmetro é um número"
+    ;;
+  [A-Z])
+    echo "O parâmetro é uma letra maiúscula"
+    ;;
+  [a-z])
+    echo "O parâmetro é uma letra minúscula"
+    ;;
+  *)
+    echo "O parâmetro é um character especial"
+esac
+    
